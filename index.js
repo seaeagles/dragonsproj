87d377f1a4f3986f824a4d03f1628b4ee5152c95
David M <dminerur@outlook.com>
2022-12-26 17:44:11 -0500

    added eventlistener to dragon button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('dragons')
+let dragon = document.getElementById('button');
+let numberOfClicks = 0;
+
+dragon.addEventListener('click', function() {
+    numberOfClicks += 1
+    console.log(numberOfClicks)
+});
+
