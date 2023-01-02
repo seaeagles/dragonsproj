43f50b44d5f482d5b46a504654367115705a9235
David M <dminerur@outlook.com>
2023-01-02 18:27:13 -0500

    modified the alert prompt

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ let numberOfClicks = 0;
 dragon.addEventListener('click', function() {
     numberOfClicks += 1,
     document.getElementById('counter').innerHTML = numberOfClicks,
-    alert(`${numberOfClicks}`)
+    alert(`You've clicked the dragon ${numberOfClicks} time(s)!`)
+    if (numberOfClicks >= 5) {
+        alert(`Now that's ${numberOfClicks} times, wow!`);
+    }
 });
