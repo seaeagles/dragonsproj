1318acfcbab714134c3847d39f018e2a333a0956
David M <dminerur@outlook.com>
2023-01-03 17:32:24 -0500

    Added a 2nd dragon and counter, removed alert, styled buttons, made responsive for mobile

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 let dragon = document.getElementById('button');
 let numberOfClicks = 0;
 
+let dragon2 = document.getElementById('button2');
+
+
+// mutating a global variable in the listener callback //
+
 dragon.addEventListener('click', function() {
     numberOfClicks += 1,
-    document.getElementById('counter').innerHTML = numberOfClicks,
-    alert(`You've clicked the dragon ${numberOfClicks} time(s)!`)
-    if (numberOfClicks >= 5) {
-        alert(`Now that's ${numberOfClicks} times, wow!`);
-    }
+    document.getElementById('counter').innerHTML = numberOfClicks
 });
+
+dragon2.addEventListener('click', function() {
+    numberOfClicks += 1,
+    document.getElementById('counter2').innerHTML = numberOfClicks
+});
+
+// dragon.addEventListener('click', function() {
+//     numberOfClicks += 1,
+//     document.getElementById('counter').innerHTML = numberOfClicks,
+//     alert(`You've clicked the dragon ${numberOfClicks} time(s)!`)
+//     if (numberOfClicks >= 5) {
+//         alert(`Now that's ${numberOfClicks} times, wow!`);
+//     }
+// });
